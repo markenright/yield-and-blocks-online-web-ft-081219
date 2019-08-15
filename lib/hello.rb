6950aa2120f613befd7308bb65e37f330f7b1de0
Mark Enright <enright.mark@gmail.com>
2019-08-15 14:26:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,5 @@
 def hello_t(array)
+  if block_given?
   i = 0
   
   while i < array.length
@@ -6,7 +7,10 @@ def hello_t(array)
     i += 1
   end
   
-array
+  array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here with an array as the argument AND with a block
