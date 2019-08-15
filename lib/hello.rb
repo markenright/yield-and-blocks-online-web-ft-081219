6ff964365a0e8c6ef696d2785a6b47ba8468e852
Mark Enright <enright.mark@gmail.com>
2019-08-15 14:18:13 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,5 +11,12 @@ end
 
 # call your method here! with an array as the argument
 
+arr = ["Tim", "Tom", "Jim"]
+
+hello_t(arr) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
 
